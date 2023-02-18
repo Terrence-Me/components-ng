e56a0248bd5fd14c8d5c1d0a3b5b05d10a2b33b4
Terrence Meikle <terrence_meikle@yahoo.com>
2023-02-18 14:07:32 -0500

    add views and stats tab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ViewsModule } from './views/views.module';
 
 const routes: Routes = [
   {
@@ -15,6 +16,11 @@ const routes: Routes = [
       import('./collections/collections.module').then(
         (m) => m.CollectionsModule
       ),
+  },
+  {
+    path: 'views',
+    loadChildren: () =>
+      import('./views/views.module').then((m) => m.ViewsModule),
   },
   {
     path: '',
