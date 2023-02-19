9219ad2a80014cd88cba8cb7741a93a70817b1c5
Terrence Meikle <terrence_meikle@yahoo.com>
2023-02-18 19:04:31 -0500

    implement modal

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-mods-home',
   templateUrl: './mods-home.component.html',
-  styleUrls: ['./mods-home.component.css']
+  styleUrls: ['./mods-home.component.css'],
 })
 export class ModsHomeComponent {
-
+  modalOpen = false;
+  onClick() {
+    this.modalOpen = !this.modalOpen;
+  }
 }
