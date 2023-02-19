3dfade3287a29ad0d3435b220054546ccf55aae4
Terrence Meikle <terrence_meikle@yahoo.com>
2023-02-18 19:58:43 -0500

    expand active element

--- src/app/mods/accordion/accordion.component.ts
+++ src/app/mods/accordion/accordion.component.ts
@@ -6,5 +6,15 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./accordion.component.css'],
 })
 export class AccordionComponent {
+  openedItemIndex = 0;
+
   @Input() data: any = [];
+
+  onClick(idx: number) {
+    if (idx === this.openedItemIndex) {
+      this.openedItemIndex = -1;
+    } else {
+      this.openedItemIndex = idx;
+    }
+  }
 }
