ff80c0cd774759b05e1654629aad2cb4456c9f11
Terrence Meikle <terrence_meikle@yahoo.com>
2023-02-18 19:47:57 -0500

    list accordion elements

--- src/app/mods/accordion/accordion.component.ts
+++ src/app/mods/accordion/accordion.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
-  styleUrls: ['./accordion.component.css']
+  styleUrls: ['./accordion.component.css'],
 })
 export class AccordionComponent {
-
+  @Input() data: any = [];
 }
--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,16 @@ import { Component, Input } from '@angular/core';
 })
 export class ModsHomeComponent {
   modalOpen = false;
+
+  items = [
+    { title: 'Why is the sky blue?', content: 'The sky is blue because it is' },
+    {
+      title: 'What does an orange tast like?',
+      content: 'An orange taste like an orange',
+    },
+    { title: 'What color is the cat?', content: 'The cat is orange' },
+  ];
+
   onClick() {
     this.modalOpen = !this.modalOpen;
   }
